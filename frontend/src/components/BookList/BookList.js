523b7b968d211a97d8899127467bbb22c8f89de4
Alona Kolisnychenko <alona.kolisnychenko.fe@gmail.com>
2023-10-16 14:10:44 +0300

    highlight filter text matches

--- frontend/src/components/BookList/BookList.js
+++ frontend/src/components/BookList/BookList.js
@@ -29,6 +29,21 @@ const BookList = () => {
     dispatch(toggleFavorite(id))
   };
 
+  const hightLightMatch = (text, filter)=>{
+    if(!filter) return text;
+    const regex = new RegExp(`(${filter})`, 'gi');
+
+    return text.split(regex).map((substring, i)=>{
+      if(substring.toLowerCase() === filter.toLowerCase()){
+        return (
+          <span key={i} className='highlight'>
+            {substring}
+          </span>
+        );
+      }
+      return substring
+    })
+  }
   return (
     <div className="app-block book-list">
       <h2>Book List</h2>
@@ -39,7 +54,7 @@ const BookList = () => {
           {filteredBooks.map((book, i) => (
             <li key={book.id}>
               <div className="book-info">
-                {++i} {book.title} by <strong>{book.author}</strong>
+                {++i} {hightLightMatch(book.title, titleFilter)} by <strong>{hightLightMatch(book.author, authorFilter)}</strong>
               </div>
               <div className="book-actions">
                 <span onClick={()=>handleToggleFavorite(book.id)}>
